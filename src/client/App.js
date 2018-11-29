911b918536d8e2eb40f9e40c35bfbaf0730ddcdc
Dave Marquess <dave.marquess@gmail.com>
2018-11-29 01:08:37 -0800

    change phrasing of opening question

--- src/client/App.js
+++ src/client/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       currentText: '',
       isAuthenticated: true,
       questionIndex: 0,
-      question: 'What is your track name?',
+      question: 'What is the name of your track?',
       responseIndex: 0,
       resultObj: {}
     };
@@ -68,7 +68,7 @@ class App extends Component {
     this.setState({
       trackArr: [],
       questionIndex: 0,
-      question: 'What is your track name?',
+      question: 'What is the name of your track?',
       responseIndex: 0
     });
   }
